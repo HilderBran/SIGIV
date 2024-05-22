6dd6d353de95006a78e146020c974cddaa1bba7c
marcosherna <marcosxdf6@gmail.com>
2024-05-21 21:57:07 -0300

    unio devraul

--- Models/Entities/SigivContext.cs
+++ Models/Entities/SigivContext.cs
@@ -60,9 +60,8 @@ public partial class SigivContext : DbContext
 
     public virtual DbSet<Usuario> Usuarios { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseMySql("server=localhost;database=SIGIV;uid=root;pwd=root", Microsoft.EntityFrameworkCore.ServerVersion.Parse("5.7.44-mysql"));
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) 
+        => optionsBuilder.UseMySql("server=localhost;database=SIGIV;uid=root;pwd=admin", Microsoft.EntityFrameworkCore.ServerVersion.Parse("5.7.44-mysql"));
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
